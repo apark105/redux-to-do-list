e21972653b97ae4f5898f2811acbd75eacd9f99d
apark105 <andypark105@gmail.com>
2018-11-16 15:46:17 -0800

    Completed Project Set-up

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux'; 
+import listReducer from './list_reducers';
+
+const rootReducer = combineReducers({
+    list: listReducer
+})
+
+export default rootReducer;
